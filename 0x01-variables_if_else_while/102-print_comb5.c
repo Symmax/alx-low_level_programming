87bffe6dd7acd255e26478efa8f8e51e422087ba
symmax <sangsymon@gmail.com>
2022-12-14 01:41:07 -0800

    task 12 Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,10 +2,10 @@
 
 /*
 * main - prints all possible combinations of two two-digit numbers
-* Return: Always 0 (Success)
+* return: Always 0 (Success)
 */
-int main(void)
-{
+	int main(void)
+	{
 	int i, j;
 
 	for (i = 0; i < 100; i++)
