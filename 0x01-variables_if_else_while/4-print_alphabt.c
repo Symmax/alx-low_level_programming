6241168c9ff986d2adbd439f22c61c8e78f5209b
symmax <sangsymon@gmail.com>
2022-12-13 18:28:24 -0800

    task 4 When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,followed by a new line, except q and e
+ * main - prints the alphabet lowercase then new line except q and e
  * Return: Aways 0 (Sucess)
  */
 int main(void)
