4e5ec392793c02ea275ae6e30085a621f38cbed5
symmax <sangsymon@gmail.com>
2022-12-13 17:26:07 -0800

    task 0 Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,7 @@
 
 
 /**
- *
- * main - assigns a random number to int n everytime
- *
- * it executes, and prints it
+ * main - assigns a random number to int n everytime it executes, and prints it
  *
  * Return: Always 0 (Success)
  *
