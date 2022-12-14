de24a68caaf4b002f2ad223ef909177cac603830
symmax <sangsymon@gmail.com>
2022-12-13 18:16:09 -0800

    task 3 alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, and then in uppercase, followed by a new line
+ * main - prints the alphabet in lowercase,uppercase then a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int ch;
