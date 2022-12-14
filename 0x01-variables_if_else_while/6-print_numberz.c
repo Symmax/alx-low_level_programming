97e7aa6b49f299de0875883fe3aee1def8ac9b07
symmax <sangsymon@gmail.com>
2022-12-13 18:35:24 -0800

    task 6 Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints single numbers base 10 from 0,then a new line putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
