2460d1dabba500eab2a647be9f8c54530abe4b97
symmax <sangsymon@gmail.com>
2022-12-13 23:54:46 -0800

    task 11 The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,12 +13,12 @@ int main(void)
 	int hundreds='0';
 
 	for (hundreds = '0'; hundreds < '9'; hundreds++)
-	{
-	for (tens = '0'; tens < '9'; tens++);
-	{
-	for (ones = '0'; ones < '9'; ones++);
-	{
-		if (!((ones == tens ) ||(tens == hundreds) || (tens > ones) || (hundreds > tens))) /*eliminates repetition*/{
+		{
+			for (tens = '0'; tens < '9'; tens++);
+			{		
+				for (ones = '0'; ones < '9'; ones++);
+				{
+				if (!((ones == tens ) ||(tens == hundreds) || (tens > ones) || (hundreds > tens))) /*eliminates repetition*/					{
 					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
