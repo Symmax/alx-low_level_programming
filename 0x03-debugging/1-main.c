24e736be3cafebac5b8c3bd82f343b8a1d1f1e01
symmax <sangsymon@gmail.com>
2022-12-19 05:11:44 -0800

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,15 +7,26 @@
 
 int main(void)
 {
-		int i;
+	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
-/*while (i < 10)
- */
-/*{*
-/*	putchar(i);*
+
+	/*while (i < 10)*/
+	/*{*
+	/*	putchar(i);*
 /*}*
- 	printf("Infinite loop avoided! \\o/\n");
+ *
+	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
+
+1-main.c:17: WARNING: Block comments use a leading /* on a separate line
+1-main.c:18: WARNING: Block comments use a leading /* on a separate line
+1-main.c:18: WARNING: Block comments use * on subsequent lines
+1-main.c:19: WARNING: Block comments use a leading /* on a separate line
+1-main.c:19: WARNING: Block comments use * on subsequent lines
+1-main.c:21: ERROR: code indent should use tabs where possible
+1-main.c:21: WARNING: please, no space before tabs
