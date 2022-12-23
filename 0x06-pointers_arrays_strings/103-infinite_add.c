5d9b84393be99b75ca1f9674ad04eb6e57dacf33
symmax <sangsymon@gmail.com>
2022-12-22 19:21:40 -0800

    task 11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -42,9 +42,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (*(n1 + i) != '\0')
 		i++;
 	while (*(n2 + j) != '\0')
-		j++;
-		i--;
-		j--;
+	j++;
+	i--;
+	j--;
 	if (j >= size_r || i >= size_r)
 	return (0);
 	while (j >= 0 || i >= 0 || overflow == 1)
@@ -55,13 +55,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			val1 = *(n1 + i) - '0';
 		if (j < 0)
 			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
-			temp_tot = val1 + val2 + overflow;
+		else val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
-			overflow = 1;
+		overflow = 1;
 		else
-			overflow = 0;
+		overflow = 0;
 		if (digits >= (size_r - 1))
 		return (0);
 			*(r + digits) = (temp_tot % 10) + '0';
