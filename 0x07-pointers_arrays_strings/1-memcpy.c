7ba77b9f717ed98d7b926ea1260297e91f738996
symmax <sangsymon@gmail.com>
2023-01-05 22:19:04 -0800

    task 2 strchr

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,8 +8,8 @@
 * Return: Always 0 (Success)
 */
 
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
+char _memcpy(char *dest, char *src, unsigned int n)
+et numbe
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
